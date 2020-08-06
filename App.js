bb552e26aecb43fb58f99eb9e941b97ce845285d
Gerardo Hidalgo <gerardo@pop-os.localdomain>
2020-08-06 15:04:10 -0600

    going through the first few steps, adding an image, base instructions, and a button

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import dice from './assets/dice.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={dice} style={styles.logo} />
+      <Text style={styles.instructions}>
+        Welcome to KillerDice
+      </Text>
+
+      <TouchableOpacity onPress={() => alert('Rolling dice...')} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a die!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,4 +25,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  logo: {
+    width: 320,
+    height: 350,
+    marginBottom: 10,
+  },
+
+  instructions: {
+    color: '#888',
+    fontSize: 30,
+    marginHorizontal: 15,
+  },
+
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
 });
